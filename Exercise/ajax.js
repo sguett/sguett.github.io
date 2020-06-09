d4819048f420ebc0d241fd05dc046f26a34867ed
Samuel Guetta <guetta.samuel@gmail.com>
2020-06-09 19:27:44 +0200

    exercise

--- Exercise/ajax.js
+++ Exercise/ajax.js
@@ -1,38 +1,54 @@
 // step 1
-let xhr = new XMLHttpRequest();
-
+let xhrJSON = new XMLHttpRequest();
+let xhrRobot = new XMLHttpRequest();
 // step 2
-xhr.open('GET', 'https://sguett.github.io/Exercise/data.json');
-
+xhrJSON.open('GET', 'https://jsonplaceholder.typicode.com/users');
+xhrRobot.open('GET', 'https://robohash.org/');
 // step 3
-xhr.send();
+xhrJSON.send();
+xhrRobot.send();
 
-xhr.responseType = 'json'; // for json file (4)
+xhrJSON.responseType = 'json'; // for json file (4)
 
-xhr.onload = function () {
+xhrJSON.onload = function () {
     if (xhr.status != 200) {
         console.log(`Error: ${xhr.status}: ${xhr.statusText}`)
     }
     else {
-        // const data = JSON.parse(xhr.response) if we don't specify (4)
-        // console.log("username=",data[1].username) if we don't specify (4)
-        for (val in xhr.response){
-            console.log(xhr.response[val].name)
-            
-        }
+        Built(xhr.response)
         // console.log(xhr.response)
     }
 }
 
-xhr.onprogress = function (event) {
-    if (event.lengthComputable) {
-        console.log(`Received ${event.loaded} of ${event.total} bytes`);
-    } else {
-        console.log(`Received ${event.loaded} bytes`); // no Content-Length
-    }
+// xhr.onprogress = function (event) {
+//     if (event.lengthComputable) {
+//         console.log(`Received ${event.loaded} of ${event.total} bytes`);
+//     } else {
+//         console.log(`Received ${event.loaded} bytes`); // no Content-Length
+//     }
 
-};
+// };
 
 xhr.onerror = function () {
     console.log("Request failed");
 };
+
+const built = (arr) => {
+    const root = document.getElementById('root')
+    arr.forEach((item,i) => {
+        let div = document.createElement("div");
+        let h1 = document.createElement("h1");
+        let p = document.createElement("p");
+        let img = document.createElement("img");
+
+        h1.innerText = item.name;
+        p.innerText = item.company.name;
+        img.setAttribute("src",`https://robohash.org/${item.id}?200x200`) // item.id or i, it's the same here
+        
+        div.appendChild(img);
+        div.appendChild(h1);
+        div.appendChild(p);
+
+        root.appendChild(div);
+    });
+}
